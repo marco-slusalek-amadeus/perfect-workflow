7e66e18f03663242a19133d431b7e590363c4d9c
Marco Slusalek <marco.slusalek@amadeus.com>
2023-04-05 12:05:42 +0200

    feat: new Headline
    I add a headline to Main page

--- src/App.svelte
+++ src/App.svelte
@@ -15,17 +15,20 @@
   </div>
   <h1>Vite + Svelte</h1>
 
+  <h1>New Headline</h1>
   <div class="card">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
